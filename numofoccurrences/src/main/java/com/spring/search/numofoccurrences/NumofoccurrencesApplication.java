a7724bf8455a97764f3794987e0e4500c7077139
Vinothkumar Jeeva <vkvinothkumar20@gmail.com>
2021-07-22 12:45:57 +0530

    Project Templated add in the repo

---
+++ numofoccurrences/src/main/java/com/spring/search/numofoccurrences/NumofoccurrencesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.search.numofoccurrences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumofoccurrencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumofoccurrencesApplication.class, args);
+	}
+
+}
---
+++ numofoccurrences/src/test/java/com/spring/search/numofoccurrences/NumofoccurrencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.search.numofoccurrences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumofoccurrencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
